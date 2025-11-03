f85825327b3774845dcb68e38ecc869be15f9619
Judy Wu <judydw@gl-login3.arc-ts.umich.edu>
2025-11-03 16:35:45 -0500

    Add run_validity_r and run_validity_nr scripts

---
+++ experiments/validity_experiments/run_validity_nr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=validity_gao_barber
+#SBATCH --output=../../logs/validity2_output.log
+#SBATCH --time=02:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=32
+
+module purge
+source /sw/pkgs/arc/python3.10-anaconda/2023.03/etc/profile.d/conda.sh
+module load R/4.3.1
+
+conda activate rpy2_env
+
+which python
+python -c "import sys; print('Python executable:', sys.executable)"
+
+export R_HOME=$(R RHOME)
+
+cd /home/judydw/SI_HierarchicalClustering
+mkdir -p logs
+mkdir -p results
+
+export PYTHONPATH=$PWD/src:$PYTHONPATH
+
+python experiments/validity_experiments/check_validity2.py
--- experiments/validity_experiments/run_validity_r.sh
+++ experiments/validity_experiments/run_validity_r.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=validity_test
 #SBATCH --output=../../logs/validity_output.log
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=32
 
 
 module purge
-module load python3.10-anaconda
+source /sw/pkgs/arc/python3.10-anaconda/2023.03/etc/profile.d/conda.sh
 conda activate rac-env
 
 cd /home/judydw/SI_HierarchicalClustering
 mkdir -p logs
-mkdir -p results/validity_experiments
+mkdir -p results
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
