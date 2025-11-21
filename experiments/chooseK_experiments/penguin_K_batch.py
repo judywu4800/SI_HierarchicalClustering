added plot_fig7 and real data plot, fixed numerical underflow in merge_inf_F_grid

experiments/chooseK_experiments/penguin_K_batch.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 import matplotlib.patches as mpatches
-from find_best_K import find_best_K_F, generate_alpha_list, get_labels_at_K, find_best_K_chi
+from find_best_K import find_best_K_F, generate_alpha_list_exp, get_labels_at_K, find_best_K_chi
 from hierarchical_clustering_invariant import *
 from palmerpenguins import load_penguins
+from alg2_one_trial2 import gap_statistic
 from joblib import Parallel, delayed
 from collections import Counter
 from datetime import datetime
@@ -18,16 +19,11 @@ parser.add_argument("--trial_id", type=int, default=0)
 args = parser.parse_args()
 
 
-def run_one_trial(seed):
-    rng = np.random.default_rng(seed)
-    K, _, _, _ = find_best_K_F(X, tau=0.1, alpha_list=alpha_list, rng=rng)
-    return K
-
 
 if __name__ == '__main__':
     seed = int(args.trial_id)
-    rng = np.random.default_rng(seed)
-
+    np.random.seed(seed)
+    random.seed(seed)
 
     penguins_raw = load_penguins()
     penguins = penguins_raw[(penguins_raw["sex"] == "female") & (penguins_raw.notna().all(axis=1)) & (
@@ -41,12 +37,14 @@ if __name__ == '__main__':
     true_K = 3
     total_alpha = 0.05
     Ks = []
-    alpha_list = generate_alpha_list(n, 0.05)
+    alpha_list = generate_alpha_list_exp(n, 0.05, decay_rate=0.5)
 
     print(f"Starting trial {seed} with n={n}")
 
     start_time = datetime.now()
-    K, _, _, _ = find_best_K_F(X, tau=0.1, alpha_list=alpha_list, rng=rng)
+    K, _, _, _ = find_best_K_F(X, tau=0.1, alpha_list=alpha_list, linkage = "average",
+                                     total_alpha=0.05, n_threshold=0.4*n, hard_threshold=0.1*n, seed = seed)
+    K_hat_gap = gap_statistic(X,K_max=30, B=50)
     end_time = datetime.now()
     elapsed_minutes = (end_time - start_time).total_seconds() / 60
 
@@ -57,5 +55,5 @@ if __name__ == '__main__':
     os.makedirs(output_dir, exist_ok=True)
 
     out_path = os.path.join(output_dir, f"K_trial_{seed}.csv")
-    pd.DataFrame({"trial_id": [seed], "K_hat": [K], "elapsed_min": [elapsed_minutes]}).to_csv(out_path, index=False)
+    pd.DataFrame({"trial_id": [seed], "K_hat": [K], "K_hat_gap": [K_hat_gap], "elapsed_min": [elapsed_minutes]}).to_csv(out_path, index=False)
 

plotting/plot_fwer.py
@@ -7,9 +7,20 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     total_alpha = 0.05
     output_dir = os.path.join("../results/figures")
-    df_results = pd.read_csv('../results/raw/fwer_results.csv')
+    import glob
+    files = glob.glob("../results/raw/fwer/fwer_tau_*.csv")
+    df_results = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
+    df_results["tau_num"] = df_results["tau"].replace("naive", 0).astype(float)
+    df_results = df_results.sort_values("tau_num")
+    print(df_results)
+
     labels = df_results["tau"].astype(str).tolist()
     x_positions = np.arange(len(labels))
+
+    xtick_labels = [
+        "Naive" if t == "naive" else rf"RAC(${t}$)"
+        for t in labels
+    ]
 
     plt.figure(figsize=(8, 5))
     non_naive_mask = df_results["tau"] != "naive"
@@ -18,7 +29,7 @@ if __name__ == "__main__":
         df_results.loc[non_naive_mask, "FWER"],
         marker='o',
         color='blue',
-        label="Empirical FWER"
+        label="Randomized Methods"
     )
     plt.scatter(
         x_positions[0],
@@ -26,14 +37,14 @@ if __name__ == "__main__":
         color='orange',
         marker='s',
         s=100,
-        label='Naive'
+        label='Naive Methods'
     )
 
-    plt.xticks(x_positions, labels)
+    plt.xticks(x_positions, xtick_labels)
     plt.axhline(y=total_alpha, color='red', linestyle='--', label=f"Alpha = {total_alpha}")
-    plt.xlabel("Tau")
+    plt.xlabel("Methods")
     plt.ylabel("FWER")
-    plt.title("FWER vs Tau (Null Data)")
+    plt.title(r"FWER vs $\tau$")
     plt.legend()
     plt.grid(True, linestyle="--", alpha=0.5)
     plt.tight_layout()

src/find_best_K.py
@@ -16,17 +16,24 @@ def generate_alpha_list(n=30, total_alpha=0.05, seed=0):
     alpha_list = total_alpha * alpha_raw / np.sum(alpha_raw)
     return alpha_list
 
-import numpy as np
-
-import numpy as np
-
-def generate_alpha_list_exp(n=30, total_alpha=0.05, decay_rate=1):
+def generate_alpha_list_exp(n=30, total_alpha=0.05, decay_rate=0.5):
     length = n - 1
     i = np.arange(length)
     alpha_raw = np.exp(-decay_rate * i)
     alpha_list = total_alpha * alpha_raw / np.sum(alpha_raw)
     return alpha_list
 
+'''
+def generate_alpha_list_exp(n=30, total_alpha=0.05, decay_rate=0.2, nonzero_count=10):
+    length = n - 1
+    k = min(nonzero_count, length)
+    i = np.arange(k)
+    raw = np.exp(-decay_rate * i)
+    alpha_nonzero = total_alpha * raw / raw.sum()
+    alpha_list = np.zeros(length)
+    alpha_list[:k] = alpha_nonzero
+    return alpha_list
+'''
 def get_labels_at_K(model, K):
     for winning_pair, clusters in model.existing_clusters_log.items():
         if len(clusters) == K:
@@ -36,7 +43,8 @@ def get_labels_at_K(model, K):
                     labels[idx] = cid
             return labels
     raise ValueError(f"No step found with {K} clusters")
-def find_best_K_F(X, tau, alpha_list, n_threshold=20, linkage="complete", total_alpha=0.05, rng = None):
+def find_best_K_F(X, tau, alpha_list, n_threshold=10, hard_threshold = 5, linkage="complete", total_alpha=0.05, seed = None):
+    rng = np.random.default_rng(seed)
     n = np.shape(X)[0]
     if not np.isclose(np.sum(alpha_list), total_alpha):
         raise ValueError(
@@ -63,22 +71,24 @@ def find_best_K_F(X, tau, alpha_list, n_threshold=20, linkage="complete", total_
         node = node1.parent
         n1 = len(node1.points)
         n2 = len(node2.points)
-        if min(n1, n2) <= n_threshold:
-            alpha = np.min(alpha_list)  # more conservative for smaller clusters
-            idx = np.argmin(alpha_list)
-
-            if (n1 + n2) == 2:
-                pval = 1  # F distribution method cannot handle the case when n1+n2=2
-            else:
-                pval, _, _ = model.merge_inference_F_grid(node, grid_width=250, ncoarse=30, ngrid=1000)
-
+        if min(n2, n2) <= hard_threshold:
+            pval = 1  # do not conduct test
+            alpha = 0
         else:
-            alpha = np.max(alpha_list)  # More power for larger clusters
-            idx = np.argmax(alpha_list)
-            pval, _, _ = model.merge_inference_F_grid(node, grid_width=250, ncoarse=30, ngrid=1000)
-        alpha_list = np.delete(alpha_list, idx)
+            if min(n1, n2) <= n_threshold:
+                alpha = np.min(alpha_list)  # more conservative for smaller clusters
+                idx = np.argmin(alpha_list)
+                pval, _, _ = model.merge_inference_F_grid(node, grid_width=300, ncoarse=20, ngrid=1000)
+
+            else:
+                alpha = np.max(alpha_list)  # More power for larger clusters
+                idx = np.argmax(alpha_list)
+                pval, _, _ = model.merge_inference_F_grid(node, grid_width=300, ncoarse=20, ngrid=1000)
+
+            alpha_list = np.delete(alpha_list, idx)
         alpha_seq.append(alpha)
         p_values.append(pval)
+        print(pval)
         if pval < alpha:
             K_hat = n - t
             labels_est = get_labels_at_K(model, K_hat)

src/hierarchical_clustering_invariant.py
@@ -822,9 +822,17 @@ class AgglomerativeClustering:
                     log_prior = f.logpdf(x=fine_grid, dfn=dfn, dfd=dfd)
                     sel_probs = interpolation.sum(axis=0)
                     log_post = log_prior + sel_probs
-                    posterior = np.exp(log_post)
+                    #posterior = np.exp(log_post)
 
-                    posterior = posterior / np.max(posterior)
+                    #posterior = posterior / np.max(posterior)
+                    log_post_shift = log_post - np.max(log_post)
+                    posterior = np.exp(log_post_shift)
+                    posterior_sum = posterior.sum()
+                    if posterior_sum == 0 or np.isnan(posterior_sum):
+                        # fallback: use uniform distribution to avoid nan
+                        posterior = np.ones_like(posterior) / len(posterior)
+                    else:
+                        posterior = posterior / posterior_sum
                     sum = 0
                     num = 0
                     for g in range(ngrid*2):