9d5eb07f1881d7aa29ef959306382a44faff9585
Judy Wu <judywu2936@gmail.com>
2026-03-25 10:02:56 -0400

    updated results and plots

--- scripts/run_typeI_r.sh
+++ scripts/run_typeI_r.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=typeI_randomK2
-#SBATCH --output=../logs/typeI_r2_output.log
+#SBATCH --job-name=typeI_random
+#SBATCH --output=../logs/typeI_r_output_%a.log
 #SBATCH --time=06:00:00
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=32
-
+#SBATCH --array=0-1
 
 module purge
 source /sw/pkgs/arc/python3.10-anaconda/2023.03/etc/profile.d/conda.sh
@@ -16,15 +16,14 @@ mkdir -p results
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 
-K=2
 NUM_TRIALS=200
 NUM_REPEATS=100
 ALPHA=0.05
+
+K_LIST=(2 3)
+K=${K_LIST[$SLURM_ARRAY_TASK_ID]}
 
 python simulations/type1_error.py \
   --K ${K} \
   --num_trials ${NUM_TRIALS} \
   --num_repeats ${NUM_REPEATS}
-
-touch results/raw/fig3/type1_done.txt
-
